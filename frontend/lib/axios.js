a82e548e8a640d1fb9438c387ffcd56b5ec315ee
Suyi-Ajayi Boluwatife <bsuyiajayi@gmail.com>
2025-06-06 15:08:14 +0100

    fix: adjust axios baseURL for production and development environments

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,8 +1,14 @@
-import axios from 'axios';
+// Create an Axios instance with dynamic baseURL:
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api/v1", // Base URL for the API
-    withCredentials: true, // Include credentials (cookies) in requests
-})
+  // - In development, point to local backend server.
+  // - In production, use relative path to the deployed backend.
+  baseURL:
+    import.meta.mode === "development"
+      ? "http://localhost:3000/api/v1"
+      : "/api/v1", // Base URL for the API
+  withCredentials: true,  // Also ensure cookies (credentials) are included in all requests.
+});
 
 export default axiosInstance;
